1493fa70f23f33529d78cc1fef7ae7024b9039bc
marc codes <marcmpescola@gmail.com>
2014-05-01 14:35:28 +0200

    Ara compila

--- classes/CharactersSet.java
+++ classes/CharactersSet.java
@@ -11,18 +11,19 @@ public class CharactersSet {
         texts = "";
     }
     public void calculateFrequency(String text) {
-        
+        int a = 3;
     }
 
     public void calculateText(String text) {
-        Vector<Character> v();
+        Vector <Character> v = new Vector <Character>();
 
         // Divideix el text en caracters
-        for (int i = 0; int increment = 0; i < text.length(); i += increment) {
+        int increment = 0;
+        for (int i = 0; i < text.length(); i += increment) {
             boolean found = false;
             int j = 0;
             while ((!found) && (j < characters.length)) {
-                string s = characters[j].getCharacter();
+                String s = characters[j].getCharacter();
                 if (text.substring(i, s.length()) == s) {
                     // S'ha trobat el caracter
                     found = true;
@@ -31,13 +32,13 @@ public class CharactersSet {
                 }
                 else ++j;
             }
-            if (not found) increment = 1;
+            if (!found) increment = 1;
         }
 
         // Recorre el text ja dividit en caracters i calcula les freqüències
         for (int i = 0; i < v.size(); ++i) {
-            r.setRelation(v[i].getId(), v[i + 1].getId(), r.getRelation(v[i], v[i + 1].getId()) + 1f);
-            r.setRelation(v[i].getId(), v[i + 2].getId(), r.getRelation(v[i], v[i + 2].getId()) + 0.5f);
+            r.setRelation(getId(v.elementAt(i)), getId(v.elementAt(i + 1)), r.getRelation(getId(v.elementAt(i)), getId(v.elementAt(i + 1))) + 1f);
+            r.setRelation(getId(v.elementAt(i)), getId(v.elementAt(i + 2)), r.getRelation(getId(v.elementAt(i)), getId(v.elementAt(i + 2))) + 0.5f);
         }
     }
 
@@ -47,14 +48,14 @@ public class CharactersSet {
 
 
     public int getId(Character c) {
-        String s = c.getName();
+        String s = c.getCharacter();
         return getId(s);
     }
 
     public int getId(String s) {
         boolean found = false;
         int i = 0;
-        while (not found) {
+        while (!found) {
             if (characters[i].getCharacter() == s) found = true;
             else ++i;
         }
@@ -69,7 +70,8 @@ public class CharactersSet {
     Pre: path es un text que es vol afegir // NO ESTIC SEGUR DE QUE FOS AIXÍ, PERO COM ENCARA NO ESTÀ EL CONTROLADOR DE HDD HO DEIXO AIXÍ DE MOMENT
     Post: Es concatena el nou text amb els textos que ja s'havien introduit. ¿Els dos textos es separen amb un caràcter reservat?
     */
-    public void addText(string path) {
-        textos.concat("SOMETHING");
-        textos.concat(path);
+    public void addText(String path) {
+        texts.concat("SOMETHING");
+        texts.concat(path);
     }
+}
