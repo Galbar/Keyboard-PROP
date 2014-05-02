d4338b7079416b03145430083fc1eafb6373e826
Albert Trelis <lordatm161@MacBook-Air-de-ATM.local>
2014-05-02 16:04:05 +0200

    fghjkl

---
+++ classes/Position.java
@@ -0,0 +1,17 @@
+
+package classes;
+
+public class Position {
+	public Float x;
+	public Float y;
+	
+	/*
+        Pre: -
+	Post: Es crea una nova instància.
+        */
+	public Position(Float x, Float y) {
+            this.x = x;
+            this.y = y;
+        }
+}
+
---
+++ classes/PositionsSet.java
@@ -0,0 +1,61 @@
+package classes;
+
+import sharedClasses.*;
+import classes.enumerations.*;
+import static java.lang.Math.sqrt;
+import java.util.*;
+import classes.Position;
+
+public class PositionsSet {
+    private final TopologyType T;
+    private Relation r;
+    private float size;
+    private final float MAX_ROW = 10;
+    private Vector<Position> p;
+    
+    private void Calculate() {
+        float dist;
+        float lastRow = size%10;
+        float totalRow = size - lastRow;
+        Vector<Pair<Float, Float>> v = new Vector<>();
+        
+        for (int i = 0; i < totalRow; ++i) {
+            int aux = (int) (i/MAX_ROW);
+            float x = aux;
+            float y = i%MAX_ROW;
+            v.add(new Pair<Float, Float>(x,y));
+        }
+        
+        float mid = lastRow/2;
+        float first = (MAX_ROW - lastRow)/2;
+        for (int i = 0; i < lastRow; ++i) {
+            int aux = (int) (size/10);
+            float x = aux;
+            float y = first + i;
+            v.add(new Pair<Float,Float>(x,y));
+        }
+        
+        for (int i = 0; i < size; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                float x = (float) Math.pow(v.get(j).second-v.get(i).second,2);
+                float y = (float) Math.pow(v.get(j).first-v.get(i).first,2);
+                dist = (float) sqrt(x+y);
+                //System.out.println("i: " + i + " j: " + j + " dist: " + dist);
+                r.setRelation(i,j,dist);
+                p.add(new Position(x,y));
+            }
+        }
+    }
+    
+    public PositionsSet(TopologyType T, int n) {
+        this.T = T;
+        size = n;
+        Vector<Position> p = new Vector<>();
+        this.p = p;
+        Calculate();
+    }
+    
+    
+   
+    
+}
