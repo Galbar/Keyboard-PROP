5aa51a2e67016bda7293aaeed948836d9bf3de56
Alessio Linares Bernardini <alessio.linares2@gmail.com>
2014-05-04 16:53:57 +0200

    Moar stuff

--- EXE/make.sh
+++ EXE/make.sh
@@ -1,22 +1,39 @@
 #! /bin/bash
 
-
+if [[ ! -f Main.java ]]; then
+	echo "copying Main class.."
+	cp -f ../FONTS/Samuel/Main.java ./
+fi
 if [[ ! -d classes/ ]]; then
 	echo "copying specific classes.."
 	mkdir classes
-	cp ../FONTS/Albert/*.java classes/
-	cp ../FONTS/Alessio/*.java classes/
-	cp ../FONTS/Marc/*.java classes/
-	cp ../FONTS/Samuel/*.java classes/
-	cp ../FONTS/Samuel/Main.java ./
+	cp -f ../FONTS/Albert/Alphabet.java classes/
+	cp -f ../FONTS/Albert/AlphabetDriver.java classes/
+	cp -f ../FONTS/Albert/Position.java classes/
+	cp -f ../FONTS/Albert/PositionDriver.java classes/
+	cp -f ../FONTS/Albert/PositionsSet.java classes/
+	cp -f ../FONTS/Albert/PositionsSetDriver.java classes/
+
+	cp -f ../FONTS/Alessio/DomainController.java classes/
+	cp -f ../FONTS/Alessio/DomainControllerDriver.java classes/
+	
+	cp -f ../FONTS/Marc/Character.java classes/
+	cp -f ../FONTS/Marc/CharacterDriver.java classes/
+	cp -f ../FONTS/Marc/CharactersSetDriver.java classes/
+	cp -f ../FONTS/Marc/CharactersSet.java classes/
+	cp -f ../FONTS/Marc/RelationDriver.java classes/
+	cp -f ../FONTS/Marc/Relation.java classes/
+
+	cp -f ../FONTS/Samuel/Keyboard.java classes/
+	cp -f ../FONTS/Samuel/KeyboardDriver.java classes/
 	mkdir classes/enumerations/
-	mv classes/TopologyType.java classes/enumerations/
-	mv classes/UsageMode.java classes/enumerations/
+	cp -f ../FONTS/Samuel/TopologyType.java classes/enumerations/
+	cp -f ../FONTS/Samuel/UsageMode.java classes/enumerations/
 fi
 if [[ ! -d sharedClasses/ ]]; then
 	echo "copying shared classes.."
 	mkdir sharedClasses
-	cp ../FONTS/COMU/*.java sharedClasses/
+	cp -f ../FONTS/COMU/*.java sharedClasses/
 fi
 
 echo "cleaning..."
--- FONTS/COMU/BoundDriver.java
+++ FONTS/COMU/BoundDriver.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sharedClasses;
-
 import java.util.Scanner;
 
 /**
- *
- * @author Sam
+ * Classe que comprova el funcionament de Bound
+ * 
+ * @author Alessio Linares
  */
 public class BoundDriver {
     public static void main(String[] args){
