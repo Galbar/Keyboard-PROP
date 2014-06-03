d434376c38b6efd283c9c553adfd4561b8ed68fe
marc codes <marcmpescola@gmail.com>
2014-06-03 13:51:50 +0200

    va fent

--- FONTS/Marc/SolutionInter.java
+++ FONTS/Marc/SolutionInter.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.util.Vector;
 import java.lang.Math.*;
+import java.awt.Color;
 
 
 /* Per dibuixar el teclat necessito:
@@ -52,7 +53,7 @@ public class SolutionInter extends JFrame {
 
     private void initializeFrame() {
         // Tamanyo
-        this.setMinimumSize(new Dimension(300, 200));
+        this.setMinimumSize(new Dimension(500, 300));
         this.setPreferredSize(this.getMinimumSize());
         this.setResizable(true);
         // Posicion y operaciones por defecto
@@ -60,7 +61,10 @@ public class SolutionInter extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Se agrega panelContenidos al contentPane (el panelContenidos se
         // podria ahorrar y trabajar directamente sobre el contentPane)
+
         JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+        //contentPane.setOpaque(false);
         contentPane.add(draw_panel);
         contentPane.add(button_panel);
     }
@@ -119,6 +123,10 @@ public class SolutionInter extends JFrame {
     }
     
     private void initializeButtonPanel(){
+        button_panel.setPreferredSize(new Dimension(450,100));
+        //button_panel.setOpaque(true);
+        //setBackground(Color.BLUE);
+
         button_panel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.weighty=1;
@@ -137,12 +145,16 @@ public class SolutionInter extends JFrame {
 	    class DrawPanel extends JPanel {
 
 	        DrawPanel() {
+                System.out.println("Construeix DrawPanel");
 	            // set a preferred size for the custom panel.
-	            setPreferredSize(new Dimension(420,420));
+	            setPreferredSize(new Dimension(450,200));
+                setOpaque(true);
+                setBackground(Color.WHITE);
 	        }
 
 	        @Override
 	        public void paintComponent(Graphics g) {
+                System.out.println("wola!");
 	            super.paintComponent(g);
 
 	            drawKeys(g);
@@ -154,9 +166,15 @@ public class SolutionInter extends JFrame {
 
 	        private void drawKeys(Graphics g) {
 	        	for (int i = 0; i < rels.size(); ++i) {
+                    System.out.println(i);
 	        		int x = Math.round(coords.get(rels.get(i)).x);
 	        		int y = Math.round(coords.get(rels.get(i)).y);
-	        		g.fillRect(x, y, 100,100); // 100 key size, may change
+                    System.out.println(x);
+                    System.out.println(y);
+                    System.out.println("--");
+                    g.setColor(Color.BLACK);
+	        		g.fillRect(x, y, 50,20); // 100 key size, may change
+                    g.setColor(Color.RED);
 	        		g.drawString(chars.get(rels.get(i)), x+10, y+10);
 	        	}
 	        }
@@ -168,6 +186,29 @@ public class SolutionInter extends JFrame {
             	Vector<String> new_chars = new Vector<String>(0);
             	Vector<Integer> new_rels = new Vector<Integer>(0);
             	Vector<Position> new_coords = new Vector<Position>(0);
+
+                new_chars.addElement("a");
+                new_chars.addElement("b");
+                new_chars.addElement("c");
+
+                new_rels.addElement(0);
+                new_rels.addElement(1);
+                new_rels.addElement(2);
+
+                Position p1 = new Position(10.0f, 10.0f);
+                Position p2 = new Position(80.0f, 10.0f);
+                Position p3 = new Position(70.0f, 50.0f);
+                new_coords.addElement(p1);
+                new_coords.addElement(p2);
+                new_coords.addElement(p3);
+
+                for (int i = 0; i < new_coords.size(); ++i) {
+                    System.out.println(new_coords.get(i).x);
+                    System.out.println(new_coords.get(i).y);
+                    System.out.println("---");
+                }
+                System.out.println("Start");
+
                 //SolutionInter sI = new SolutionInter.getInstance(new_chars, new_rels, new_coords);
                 SolutionInter sI = new SolutionInter(new_chars, new_rels, new_coords);
             }
